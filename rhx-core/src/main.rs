3848e47b447ceb455a5bea45bad6814a02a05799
Mattia Marini <marmattia26@gmail.com>
2026-08-07 20:21:10 +0200

    feat(core): this should break

--- rhx-core/src/main.rs
+++ rhx-core/src/main.rs
@@ -2,6 +2,6 @@ fn main() {
     println!("Hello, world!");
 }
 
-pub fn useful_function() -> String {
+pub fn useful_function(breaking: u32) -> String {
     "This is a useful function".to_string()
 }
